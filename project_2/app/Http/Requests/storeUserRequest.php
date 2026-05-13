190d46d3155872d8149f75f29259889c6cedde7b
Blank-16 <ansumanpal16@gmail.com>
2026-05-13 23:12:41 +0530

    ..

--- project_2/app/Http/Requests/storeUserRequest.php
+++ project_2/app/Http/Requests/storeUserRequest.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\checkUpperCase;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Override;
 
 class storeUserRequest extends FormRequest
 {
@@ -12,7 +14,7 @@ class storeUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,7 +25,51 @@ class storeUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'StudentName' => ['required', 'alpha', 'min:3'],
+            'email' => ['required', 'email', 'unique'],
+            'mobile' => ['required', 'numeric', 'digits:10'],
+            'alt_mobile' => ['nullable', 'numeric'],
+            'gender' => ['required'],
+            'dob' => ['required', 'date'],
+            'age' => ['required', 'numeric', 'min:17'],
+            'address' => ['required'],
+            'pincode' => ['required', 'numeric', 'digits:6'],
+            'course' => ['required'],
+            'percentage' => ['required', 'numeric', 'between:0,100'],
+            'signature' => ['required', 'file', 'mimes:jpeg,png,jpg,pdf'],
+            'password' => ['required', 'min:8'],
+            'confirm_password' => ['same:password'],
+            'terms' => ['required', 'accepted']
+
+        ];
+    }
+
+    // public function attributes()
+    // {
+    //     return [
+    //         'email'=>'email address',
+    //         'username'=>'First Name',
+    //     ];
+    // }
+
+    public function messages()
+    {
+        return [
+            'StudentName.required' => 'student name is require',
+            'email.email' => 'Please enter a valid email format',
+            'mobile.digits' => 'Mobile number must contain 10 digits',
+            'alt_mobile.numeric' => 'Alternate mobile number should contains only numbers',
+            'gender.required' => 'Please select your gender',
+            'dob.date' => 'Please enter valid date of birth',
+            'age.min' => 'Student must be at least 17 years old',
+            'address.required' => 'Address field cannot be empty',
+            'pincode.digits' => 'Pincode must contain 6 digits',
+            'course.required' => 'Please select a course',
+            'percentage.between' => 'Marks must be between 0 and 100',
+            'signature.mimes' => 'Please upload valid signature file',
+            'password.min' => 'Password must contain at least 8 characters',
+            'confirm_password.same' => 'Passwords do not match',
+            'terms.accepted' => 'You must accept terms and conditions',
         ];
     }
 }
--- project_2/resources/views/form.blade.php
+++ project_2/resources/views/form.blade.php
@@ -1,17 +1,45 @@
-<form action="/submit-form" method="get">
+<form action="/submit-form" method="post" enctype="multipart/form-data">
     @csrf
-    @method('delete')
-    Username: <input type="text" name="username">
-    <br><br>
-    Email: <input type="text" name="email">
-    <br><br>
+
+    Student Name: <input type="text" name="StudentName" value="{{ old('StudentName') }}">
+    <br> <br>
+    Email: <input type="text" name="email" value="{{ old('email') }}">
+    <br> <br>
+    Mobile Number: <input type="text" name="mobile" value="{{ old('mobile') }}">
+    <br> <br>
+    Alternate Mobile: <input type="text" name="alt_mobile" value="{{ old('alt_mobile') }}">
+    <br> <br>
+    Gender:
+    <input type="radio" name="gender" value="male" {{ old('gender') == 'male' ? 'checked' : '' }}> Male
+    <input type="radio" name="gender" value="female" {{ old('gender') == 'female' ? 'checked' : '' }}> Female
+    <br> <br>
+    Date of Birth: <input type="date" name="dob" value="{{ old('dob') }}">
+    <br> <br>
+    Age: <input type="number" name="age" value="{{ old('age') }}">
+    <br> <br>
+    Address: <textarea name="address">{{ old('address') }}</textarea>
+    <br> <br>
+    Pincode: <input type="text" name="pincode" value="{{ old('pincode') }}">
+    <br> <br>
+    Course: <input type="text" name="course" value="{{ old('course') }}">
+    <br> <br>
+    Percentage/Marks: <input type="text" name="percentage" value="{{ old('percentage') }}">
+    <br> <br>
+    Signature upload: <input type="file" name="signature">
+    <br> <br>
+    Password: <input type="password" name="password">
+    <br> <br>
+    Confirm Password: <input type="password" name="confirm_password">
+    <br> <br>
+    Terms and Conditions: <input type="checkbox" name="terms" {{ old('terms') ? 'checked' : '' }}>
+    <br> <br>
     <button type="submit">Submit</button>
 </form>
-@error('username')
+
 @if($errors->any())
 <ul>
-    @foreach($errors->all() as $err)
-    <li>{{$err}}</li>
+    @foreach($errors->all() as $error)
+    <li>{{$error}}</li>
     @endforeach
 </ul>
-@enderror
+@endif
